fe412b45274545d87189bdaab8971a1b1ca353be
cheolminJOO <cheolmin.joo@gmail.com>
2024-01-20 20:13:35 +0900

    [add] 전체페이지 넘기기 기능

--- src/test/Introduction.tsx
+++ src/test/Introduction.tsx
@@ -1,18 +1,15 @@
-
-import { FullPage, Slide } from 'react-full-page';
-
 export default function Introduction() {
   return (
-    <FullPage>
-      <Slide>
+    <div>
+      <div>
         <h1>저는 주철민입니다. 하고 싶은 게 많습니다</h1>
-      </Slide>
-      <Slide>
+      </div>
+      <div>
         <h1>Another slide content</h1>
-      </Slide>
-      <Slide>
+      </div>
+      <div>
         <h1>Another slide content</h1>
-      </Slide>
-    </FullPage>
+      </div>
+    </div>
   );
 }
