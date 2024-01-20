15d5ba53e8cafac8bc6dc43387df7e265e6e5493
cheolminJOO <cheolmin.joo@gmail.com>
2024-01-20 18:16:54 +0900

    [test] 코드 테스트 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import './App.css';
-
+import { FullPage, Slide } from 'react-full-page';
 
 function App() {
-  return <div>안녕하세요</div>;
+  return (
+    <FullPage>
+      <Slide>
+        <h1>저는 주철민입니다.</h1>
+      </Slide>
+      <Slide>
+        <h1>Another slide content</h1>
+      </Slide>
+      <Slide>
+        <h1>Another slide content</h1>
+      </Slide>
+    </FullPage>
+  );
 }
 
 export default App;
